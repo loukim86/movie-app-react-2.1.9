39529cd4cc1dd79bb0c9e6b925e7d42b10f9ed19
loukim86 <148028680+loukim86@users.noreply.github.com>
2024-02-02 14:56:51 +0900

    Update MovieService.js

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,7 +1,8 @@
-import SomeError from './SomeError';
+import SomeError from './SomeError'
 
 export default class MovieService {
-  _moviePopularUrl = 'https://api.themoviedb.org/3';
+  _moviePopularUrl = 'https://api.themoviedb.org/3'
+  _searchUrl = 'https://api.themoviedb.org/3/search/movie'
 
   _options = {
     method: 'GET',
@@ -11,21 +12,21 @@ export default class MovieService {
         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWU3NWY0MGY4MGVlZTZjOWJkMWQxMGYyNTNjZTU2YiIsInN1YiI6IjY1YjdhMGE4YTI4NGViMDEzMDBhNGU2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WdAJs01TDV6T1KL5uQ-sWRNFf0BZREXtGuHVjIPg5oo',
     },
     withCredentials: true,
-  };
-  _apiKey = '41e75f40f80eee6c9bd1d10f253ce56b';
+  }
+  _apiKey = '41e75f40f80eee6c9bd1d10f253ce56b'
 
-  async getAllMovies(pageNumber = 1) {
+  async searchMovies(query) {
     try {
-      const movieUrl = `${this._moviePopularUrl}/movie/popular?language=en-US&page=${pageNumber}`;
-      const res = await fetch(movieUrl, this._options);
+      const searchUrl = `${this._searchUrl}?language=en-US&page=1&query=${query}&api_key=${this._apiKey}&include_adult=false`
+      const res = await fetch(searchUrl, this._options)
       if (!res.ok) {
-        throw new SomeError('Ошибка ' + res.status);
+        throw new SomeError('Ошибка ' + res.status)
       }
-      const response = await res.json();
+      const response = await res.json()
 
-      return response.results;
+      return response.results
     } catch (error) {
-      return error;
+      return error
     }
   }
 }
